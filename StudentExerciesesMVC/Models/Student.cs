7eb7d051cbc070fd7477dc973333ff2932f1950c
deliotclarke <deliotclarke@gmail.com>
2019-08-12 14:09:56 -0500

    Add project files.

---
+++ StudentExerciesesMVC/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExerciesesMVC.Models
+{
+    public class Student
+    {
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string SlackHandle { get; set; }
+        [Required]
+        public int CohortId { get; set; }
+        public String FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
