7e0c264b3ce84d4102ce2d46c5b208fa8108f287
deliotclarke <deliotclarke@gmail.com>
2019-08-12 14:49:02 -0500

    added student model, spun up MVC

--- StudentExerciesesMVC/Models/Student.cs
+++ StudentExerciesesMVC/Models/Student.cs
@@ -9,15 +9,21 @@ namespace StudentExerciesesMVC.Models
     public class Student
     {
         [Required]
+        [Display(Name = "Student ID")]
         public int StudentId { get; set; }
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
+        [Display(Name = "Slack Handle")]
         public string SlackHandle { get; set; }
         [Required]
+        [Display(Name = "Cohort ID")]
         public int CohortId { get; set; }
+        [Display(Name = "Full Name")]
         public String FullName
         {
             get
